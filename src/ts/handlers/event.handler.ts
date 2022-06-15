0d8422cdda06cdc8d679775fbbc38c0cd17fc16c
Johan Maximus <maxi4220@hotmail.com>
2022-06-15 01:21:21 -0300

    Following a tutorial

--- src/ts/handlers/event.handler.ts
+++
@@ -1,1 +0,0 @@
-export const EventHandler = new Phaser.Events.EventEmitter();
--- webpack.config.js
+++ webpack.config.js
@@ -1,66 +1,23 @@
 /*global __dirname */
 /* eslint-disable @typescript-eslint/no-var-requires */
 const path = require('path');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-	entry: {
-		app: './src/ts/game.ts'
-	},
+	entry: "./src/game.ts",
 	module: {
-		rules: [{
-			test: /\.tsx?$/,
-			use: 'ts-loader',
-			exclude: /node_modules/
-		}]
-	},
-	resolve: {
-		extensions: [ '.tsx', '.ts', '.js' ]
-	},
-	externals: {
-		phaser: 'Phaser'
+		rules: [
+			{
+				test: /\.ts$/,
+				use: "ts-loader",
+				include: [
+					path.resolve(__dirname, "src")
+				]
+			}
+		]
 	},
 	output: {
-		filename: 'app.js',
-		path: path.resolve(__dirname, 'public')
+		filename: "bundle.js",
+		path: path.resolve(__dirname, "public")
 	},
-	devtool: "source-map",
-	plugins: [
-		new CleanWebpackPlugin(),
-		new CopyWebpackPlugin({
-			patterns: [
-				{
-					from: './src/*.html',
-					to: '[name][ext]'
-				},
-				{
-					from: './src/*.js',
-					to: '[name][ext]'
-				},
-				{
-					from: './src/*.css',
-					to: '[name][ext]'
-				},
-				{
-					from: './src/*.ico',
-					to: '[name][ext]'
-				},
-				{
-					from: './src/assets',
-					to: 'assets',
-					globOptions: {
-						ignore: [ '*.md' ]
-					}
-				}
-			]
-		})
-	],
-	devServer: {
-		open: true
-	},
-	
-	experiments: {
-		topLevelAwait: true,
-	}
-};
+	mode: "development"
+}
